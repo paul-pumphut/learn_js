9f2458d4ca75cab5260b9d1535028afc6e943758
pumphut <paul.pumphut@gmail.com>
2023-01-13 14:55:48 +0300

    Learning Parcel #1
    Learning Parcel bundler building pipeline

---
+++ ParcelBundlerDemo/first.js
@@ -0,0 +1,17 @@
+const x = 1;
+
+function addOne(n) {
+	return x + n;
+}
+
+
+function sum(a, b) {
+	return a + b;
+}
+
+
+
+export {
+	sum,
+	addOne
+}
---
+++ ParcelBundlerDemo/index.js
@@ -0,0 +1,7 @@
+import { sum, addOne } from './first';
+import { firstName } from './second';
+
+
+console.log('Hello from entry point ', firstName);
+console.log(addOne(3));
+console.log(sum(2, 3));
